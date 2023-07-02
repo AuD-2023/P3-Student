9481e776380b0f2bc53012273639f4d21b542893
LukasKlenner <klenner-lukas@t-online.de>
2023-07-02 18:18:51 +0200

    Add title to selectionScene; increase button size; decrease scene height

--- src/main/java/p3/gui/SelectionScene.java
+++ src/main/java/p3/gui/SelectionScene.java
@@ -1,9 +1,15 @@
 package p3.gui;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 /**
  * A scene for selecting which of the two algorithms should be displayed.
@@ -15,15 +21,24 @@ public class SelectionScene extends AnimationScene {
     final Button kruskalButton = new Button("Kruskal");
 
     public SelectionScene() {
-        super(new StackPane());
-        StackPane root = (StackPane) getRoot();
-        root.setPrefSize(700, 700);
+        super(new BorderPane());
+        BorderPane root = (BorderPane) getRoot();
+        root.setPrefSize(700, 200);
 
-        VBox vBox = new VBox(10);
-        vBox.getChildren().addAll(kruskalButton, dijkstraButton);
-        vBox.setAlignment(Pos.CENTER);
+        kruskalButton.setPrefSize(100, 50);
+        dijkstraButton.setPrefSize(100, 50);
 
-        root.getChildren().add(vBox);
+        HBox hBox = new HBox(10);
+        hBox.getChildren().addAll(kruskalButton, dijkstraButton);
+        hBox.setAlignment(Pos.CENTER);
+
+        root.setCenter(hBox);
+
+        Label title = new Label("Select Which Algorithm To Animate");
+        title.setStyle("-fx-font: 24 arial;");
+        title.setPadding(new Insets(20, 0, 0 ,0));
+        BorderPane.setAlignment(title, Pos.CENTER);
+        root.setTop(title);
     }
 
     @Override
